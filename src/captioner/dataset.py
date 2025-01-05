ddc56bace40a079a6c5ad5217a35c24c177abeb1
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-01-05 01:56:35 +0100

    simplify dataset streaming by removing unnecessary processing loop

--- src/captioner/dataset.py
+++ src/captioner/dataset.py
@@ -13,9 +13,4 @@ class DatasetManager:
     def stream_dataset(dataset_name, split,preprocess,num_samples=1):
         ds = datasets.load_dataset(dataset_name,split=split,streaming=True)
         ds  = ds.take(num_samples)
-        processed_samples = []
-
-        for sample in tqdm.tqdm(ds, total=num_samples, desc="Processing dataset"):
-            processed_sample = preprocess(sample)
-            processed_samples.append(processed_sample)
-        return processed_samples
+        return ds
