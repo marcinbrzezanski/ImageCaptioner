6bc7c60da84b29da39bf80ad00a9af418158a5aa
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-01-07 20:54:50 +0100

    refactor stream_dataset method to remove unused preprocess parameter

--- src/captioner/dataset.py
+++ src/captioner/dataset.py
@@ -9,7 +9,7 @@ class DatasetManager:
             function=preprocess,
             batched=True)
     @staticmethod
-    def stream_dataset(dataset_name, split,preprocess,num_samples=1):
+    def stream_dataset(dataset_name, split,num_samples=1):
         ds = datasets.load_dataset(dataset_name,split=split,streaming=True)
         ds  = ds.take(num_samples)
         return ds
