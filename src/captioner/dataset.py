03284c43521b91ef65b0d3f2b0ad65e6bdda3e99
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-01-16 20:21:52 +0100

    select specific columns from dataset in stream_dataset method

--- src/captioner/dataset.py
+++ src/captioner/dataset.py
@@ -11,5 +11,6 @@ class DatasetManager:
     @staticmethod
     def stream_dataset(dataset_name, split,num_samples=1):
         ds = datasets.load_dataset(dataset_name,split=split,streaming=True)
+        ds.select_columns(["labels","pixel_values"])
         ds  = ds.take(num_samples)
         return ds
