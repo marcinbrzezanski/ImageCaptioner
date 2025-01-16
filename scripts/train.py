3db8936404eb0976b228d3a744a813ffd5c4d028
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-01-16 21:09:48 +0100

    import

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 from transformers import AdamW, get_scheduler
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 from captioner.model import ImageCaptionerModel
 from captioner.preprocessor import DataPreprocessor
