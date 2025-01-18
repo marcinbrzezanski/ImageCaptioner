5072f24ec97bacf584415ce2f15daa27e8c31323
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-01-18 21:31:34 +0100

    Remove unused tokenizer and feature extractor from ImageCaptionerModel

--- src/captioner/model.py
+++ src/captioner/model.py
@@ -3,13 +3,13 @@ from transformers import VisionEncoderDecoderModel, AutoTokenizer, ViTImageProce
 class ImageCaptionerModel:
     def __init__(self, encoder_model: str, decoder_model: str):
         self.model = VisionEncoderDecoderModel.from_pretrained(encoder_model)
-        self.tokenizer = AutoTokenizer.from_pretrained(decoder_model)
-        self.feature_extractor = ViTImageProcessor.from_pretrained(encoder_model)
+        #self.tokenizer = AutoTokenizer.from_pretrained(decoder_model)
+        #self.feature_extractor = ViTImageProcessor.from_pretrained(encoder_model)
 
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.model.config.eos_token_id = self.tokenizer.eos_token_id
-        self.model.config.decoder_start_token_id = self.tokenizer.bos_token_id
-        self.model.config.pad_token_id = self.tokenizer.pad_token_id
+        #self.tokenizer.pad_token = self.tokenizer.eos_token
+        #self.model.config.eos_token_id = self.tokenizer.eos_token_id
+        #self.model.config.decoder_start_token_id = self.tokenizer.bos_token_id
+        #self.model.config.pad_token_id = self.tokenizer.pad_token_id
 
     def get_model_components(self):
-        return self.model, self.tokenizer, self.feature_extractor
+        return self.model, #self.tokenizer, self.feature_extractor
