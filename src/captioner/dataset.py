daefe88f31fe9cdb290f89bf48c97e35574198b6
marcinbrzezanski <marcin.brzezanski@wp.pl>
2025-05-03 15:55:39 +0200

    Make preprocess_func optional in load_dataset method

--- src/captioner/dataset.py
+++ src/captioner/dataset.py
@@ -7,11 +7,12 @@ class DatasetManager:
     def __init__(self, batch_size=4, num_workers=4):
         self.batch_size = batch_size
         self.num_workers = num_workers
-    def load_dataset(self, dataset_name, split, preprocess_func):
+    def load_dataset(self, dataset_name, split, preprocess_func=None):
         ds = datasets.load_dataset(dataset_name,split=split)
-        ds = ds.map(
-            function=preprocess_func,
-            batched=True)
+        if preprocess_func is not None:
+            ds = ds.map(
+                function=preprocess_func,
+                batched=True)
         return DataLoader(ds, batch_size=self.batch_size, collate_fn=default_data_collator, num_workers=self.num_workers)
     def stream_dataset(self, dataset_name, split,num_samples=1):
         ds = datasets.load_dataset(dataset_name,split=split,streaming=True)
